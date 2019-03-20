4050ed18da13593491a5b337c6ab193d2c69305e
Mathias Fuchs <mathias2975@gmail.com>
2019-03-20 17:53:03 +0100

    binom coeff in new file

---
+++ core/binomCoeff.c
@@ -0,0 +1,20 @@
+#include "binomCoeff.h"
+#include <limits.h>
+
+static rr binomialCoefficient(size_t n, size_t k) {
+	rr b;
+	if (k == 0) {
+		b.i = 1; b.isInfty = 0; return b;
+	}
+	if (k == 1) {
+		b.i = n; b.isInfty = 0; return b;
+	}
+	if (k > n / 2) return binomialCoefficient(n, n - k);
+	rr o = binomialCoefficient(n - 1, k - 1);
+	if (o.isInfty || o.i > INT_MAX / n * k) {
+		b.isInfty = 1; return b;
+	}
+	b.i = o.i * n / k;
+	b.isInfty = 0;
+	return b;
+}
---
+++ core/binomCoeff.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stddef.h>
+
+// struct to keep track of whether an unsigned int is beyond the bounds
+typedef struct {
+	unsigned int i;
+	int isInfty;
+} rr;
+
+// binomialCoefficient unless second entry indicates it's too big
+static rr binomialCoefficient(size_t n, size_t k);
